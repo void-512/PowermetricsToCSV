0fcc58c3a88d775e159a5ca832141bb690610ff3
Tony Wang <t85wang@uwaterloo.ca>
2025-12-22 12:51:45 -0500

    export to csv

--- main.cpp
+++ main.cpp
@@ -3,8 +3,29 @@
 #include "Sample.hpp"
 #include "FileReader.hpp"
 
+void exportToCsv(const std::vector<MetricsSample>& parsedData,
+                 const std::string& filePath)
+{
+    std::ofstream file(filePath);
+    if (!file.is_open()) {
+        throw std::runtime_error("Failed to open CSV file");
+    }
+
+    file << "Timestamp,CPU Power (mW),GPU Power (mW),ANE Power (mW),Combined Power (mW)\n";
+    for (const auto& sample : parsedData) {
+        file << std::chrono::system_clock::to_time_t(sample.getTimestamp()) << ","
+             << sample.getCpuPowerMw() << ","
+             << sample.getGpuPowerMw() << ","
+             << sample.getAnePowerMw() << ","
+             << sample.getCombinedPowerMw() << "\n";
+    }
+
+    file.close();
+}
+
+
 int main(int argc, char* argv[]) {
-    FileReader fileReader("sample_long.txt");
+    FileReader fileReader("singlesample.txt");
     std::vector<MetricsSample> parsedData;
     ParseTask parseTask(parsedData);
     fileReader.start();
@@ -13,5 +34,6 @@ int main(int argc, char* argv[]) {
         parseTask.parse(task);
         std::cout << parsedData.back() << std::endl;
     }
+    exportToCsv(parsedData, "output.csv");
     return 0;
 }
